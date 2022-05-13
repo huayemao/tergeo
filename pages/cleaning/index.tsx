f413004bea26e3082315e549fb03332b2b598aee
huayemao <huayemao4g@gmail.com>
2022-05-13 13:24:27 +0800

    计时器入口卡片； CleaningCategory 类型的问题；

--- pages/cleaning/index.tsx
+++ pages/cleaning/index.tsx
@@ -8,54 +8,53 @@ import { TextInput } from '../../components/common/FormControls/TextInput'
 import Layout from '../../components/Layout'
 
 type CleaningCategory = {
-  label: 'string'
-  color: 'string'
+  label: string
+  color: string
+  value: string
   to: UrlObject
 }
 
 const categories: CleaningCategory[] = [
   {
     label: '刷牙',
-    key: 'brush',
+    value: 'brush',
     color: 'text-indigo-600',
     to: { pathname: 'cleaning/timer', query: { type: 'brush' } },
   },
   {
     label: '使用牙线',
     color: 'text-blue-600',
-    key: 'floss',
+    value: 'floss',
     to: { pathname: 'cleaning/timer', query: { type: 'floss' } },
   },
   {
     label: '漱口',
     color: 'text-green-600',
-    key: 'rinse ',
+    value: 'rinse',
     to: { pathname: 'cleaning/timer', query: { type: 'rinse' } },
   },
 ]
 
-const Card = ({ color, label, to, key }: CleaningCategory) => (
-  <a
-    href=""
-    className={
-      'group flex flex-col justify-between rounded-sm bg-white p-8 shadow-xl transition-shadow hover:shadow-lg'
-    }
-  >
-    <div>
-      <h5 className={'text-5xl font-bold text-indigo-600 ' + color}>{label}</h5>
-      <div className="mt-4 border-t-2 border-indigo-100 pt-2">
-        <p className="text-sm font-medium uppercase tracking-widest text-gray-500">
-          已完成 2/3
-        </p>
+const Card = ({ color, label, to, value }: CleaningCategory) => (
+  <Link href={to}>
+    <a
+      className={
+        'group flex flex-col justify-between  rounded-xl bg-white p-8 shadow-xl transition-shadow hover:shadow-lg'
+      }
+    >
+      <div>
+        <h5 className={'text-3xl font-bold text-indigo-600 ' + color}>
+          {label}
+          {'  '}<sub className="font-light text-gray-600">{value}</sub>
+        </h5>
+        <div className="mt-4 border-t-2 border-indigo-100 pt-2">
+          <p className="text-sm font-medium uppercase tracking-widest text-gray-500">
+            已完成 2/3
+          </p>
+        </div>
       </div>
-    </div>
-    <Link href={to}>
-      <a className={'mt-8 inline-flex items-center text-indigo-600 ' + color}>
-        <span className="text-lg font-medium">去完成</span>
-        <ArrowNarrowRightIcon className="ml-3 h-6 w-6 transform transition-transform group-hover:translate-x-3"></ArrowNarrowRightIcon>
-      </a>
-    </Link>
-  </a>
+    </a>
+  </Link>
 )
 
 const Me: NextPage = ({ data }) => {
@@ -65,7 +64,7 @@ const Me: NextPage = ({ data }) => {
         <div className="p-8">
           <ul className="space-y-7">
             {categories.map((e) => (
-              <Card {...e} key={e.label}></Card>
+              <Card {...e} key={e.value}></Card>
             ))}
           </ul>
         </div>
